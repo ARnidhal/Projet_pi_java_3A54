f0599bcc53e4f2a62a051919ece627c2790beec2
Mohamed Rayen Toujani <mo.rayen.toujani@gmail.com>
2024-05-05 12:23:47 +0100

    integrated user blog service rendezvous

---
+++ src/main/java/com/visita/services/IService.java
@@ -0,0 +1,18 @@
+package com.visita.services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IService<T> {
+
+
+    public void ajouter(T t);
+    public void  supprimer(T t);
+    public void modifier(T t);
+    public void block(T t);
+    public void unblock(T t);
+    public void switchToUser(T t);
+    public void switchToAdmin(T t);
+    public List<T> afficher() throws SQLException;
+
+}
